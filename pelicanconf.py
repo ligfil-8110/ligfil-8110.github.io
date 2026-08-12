1d97699107f20059c30c584e7f0de81e1445e322
ligfil-8110 <ligfil.blog@gmail.com>
2026-08-13 01:12:07 +0900

    blog: ブログ改修

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,8 @@
 AUTHOR = 'みっちー'
 SITENAME = 'みっちーのブログ'
-SITEURL = "https://ligfil-8110.github.io"
+# Local previews must load CSS, JavaScript, and images from the local server.
+# Production overrides this in publishconf.py.
+SITEURL = ""
 SITE_DESCRIPTION = 'ゲーム、ランニング、PC、日々の気づきを自分の言葉で記録するブログです。'
 GOOGLE_ANALYTICS_ID = 'G-JDKE8JVXLC'
 
