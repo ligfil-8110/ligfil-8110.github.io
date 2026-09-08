48b3a7b2465bde67e632ac3c14cdf4f1d7d4cc4a
ligfil-8110 <ligfil.blog@gmail.com>
2026-09-08 22:49:44 +0900

    Clarify CHUWI recovery guidance and publish robots.txt

--- publishconf.py
+++ publishconf.py
@@ -7,6 +7,16 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
+# Production-only static files that must be published at the site root.
+STATIC_PATHS = [*STATIC_PATHS, "extra/robots.txt"]
+EXTRA_PATH_METADATA = {
+    **EXTRA_PATH_METADATA,
+    "extra/robots.txt": {"path": "robots.txt"},
+}
+
+# Use a stable Japanese date format instead of relying on the build host locale.
+DEFAULT_DATE_FORMAT = "%Y年%m月%d日"
+
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://ligfil-8110.github.io"
 RELATIVE_URLS = False
